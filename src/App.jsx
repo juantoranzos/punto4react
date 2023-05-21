17fd8cac0ee4a46dbe1901cb8d2bb08a2dd4d65a
juantoranzos <toranzos628@gmail.com>
2023-05-21 17:30:08 -0300

    instale react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <h1>titulo de prueba</h1>
     </>
   )
 }
